a27ac3d6dcc0ee69b137a9e2f3b098e9b2c09cfc
Gerard Fernandez Tejada <84667512+Gefete@users.noreply.github.com>
2023-07-23 20:43:11 +0200

    add put in service

--- src/app/home/form-create-character/form-create-character.component.ts
+++ src/app/home/form-create-character/form-create-character.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Character } from 'src/app/model/character';
 import { RickmortyService } from 'src/app/rickmorty.service';
 
@@ -7,7 +7,7 @@ import { RickmortyService } from 'src/app/rickmorty.service';
   templateUrl: './form-create-character.component.html',
   styleUrls: ['./form-create-character.component.css']
 })
-export class FormCreateCharacterComponent {
+export class FormCreateCharacterComponent implements OnInit{
   
   id! : string;
   name! : string;
@@ -17,9 +17,25 @@ export class FormCreateCharacterComponent {
   origin! : string;
   image! : string;
 
-  constructor(private rickservice : RickmortyService){}
+  characters : any | undefined ;
 
+  constructor(private rickservice : RickmortyService){}
+  ngOnInit(): void {
+    if (this.characters==undefined) {
+      // Llama al metodo que genera aleatorios
+      /* this.rickservice.setRandomized(); */
+      // Llama al metodo para recibir los datos de la API
+      this.rickservice.getAll()
+      .subscribe(result => {
+        // imprime por consola datos recibidos
+        console.warn(result);
+        // guarda los datos en el array de este componente
+        this.characters = result;
+      });
+    }
+  }
   createNewChar(){
+    // Crea el objeto para enviarlo al service
     let character : Character ={
       id:this.id,
       name:this.name,
@@ -29,14 +45,32 @@ export class FormCreateCharacterComponent {
       origin:this.origin,
       image:this.image
     }
-    this.rickservice.create(character)
-      .subscribe(
-        response => {
-          console.log(response);
-        },
-        error => {
-          console.log(error)
-        }
-      );
+    // Si el Id es encontrado en el array obtenido del json lo modifica (utilizamos find metodo del array para buscar resultados)
+    if(this.characters.find((result: { id: any; }) =>(result.id == character.id))){
+      this.rickservice.update(character.id, character)
+        .subscribe(
+          response => {
+            console.log(response);
+            // Muestra el mensaje para hacer saber si ha realizado una actualización
+            console.log("Actualizado");
+          },
+          error => {
+            console.log(error)
+          }
+        );
+    // Si el id no es encontrado crea el caracter
+    }else{
+      this.rickservice.create(character)
+        .subscribe(
+          response => {
+            console.log(response);
+            // Muestra el mensaje para hacer saber si ha realizado la creacion
+            console.log("Creado");
+          },
+          error => {
+            console.log(error)
+          }
+        );
+    }
   }
 }
--- src/app/rickmorty.service.ts
+++ src/app/rickmorty.service.ts
@@ -12,20 +12,29 @@ export class RickmortyService {
   
   constructor( private http:HttpClient ) {}
   
+  // Metodo para Recoger los datos del json
   getAll():Observable<Character[]>{
     return this.http.get<Character[]>(baseUrl);
   }
 
+  // Metodo para recoger un objeto del json
   get(id:any){
     return this.http.get(`${baseUrl}/${id}`);
   }
 
+  // Metodo para crear un objeto en el json
   create(data:Character):Observable<any>{
     return this.http.post(baseUrl,data);
   }
 
+  // Metodo para borrar un objeto en el json
   delete(id:Character):Observable<any>{
     return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  // Metodo para actualizar el json
+  update(id:Character, data: any):Observable<any>{
+    return this.http.put(`${baseUrl}/${id}`, data);
   }
 }
 
